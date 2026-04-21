f91fe840fbc80fa599bab82712c62cde32390525
syu1202silverbullet <syu1202silverbullet@gmail.com>
2026-04-21 19:19:46 +0900

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "GAS Recipe｜Google Apps Scriptで日常を自動化";
+export const SITE_DESCRIPTION = "Google Apps Scriptで作る「毎日使える自動化ツール」の実例集。業務効率化・副業・個人開発のヒントをお届けします。";
